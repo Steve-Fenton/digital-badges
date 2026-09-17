7b98186a3c28882ab398f00a9cc2a321603d5d72
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2026-09-17 14:15:39 +0100

    Version bump. Works with WP 7.1

--- fenton-digital-badges.php
+++ fenton-digital-badges.php
@@ -3,8 +3,9 @@
  * Plugin Name:       Fenton Digital Badges
  * Plugin URI:        https://github.com/Steve-Fenton/digital-badges
  * Description:       Issue, manage, and display digital badges.
- * Version:           0.1.37
+ * Version:           0.1.38
  * Requires at least: 6.2
+ * Tested up to:      7.1
  * Requires PHP:      8.0
  * Author:            Fenton
  * License:           GPL-2.0-or-later
@@ -21,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'FENDIGIBADGE_VERSION', '0.1.37' );
+define( 'FENDIGIBADGE_VERSION', '0.1.38' );
 define( 'FENDIGIBADGE_FILE', __FILE__ );
 define( 'FENDIGIBADGE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FENDIGIBADGE_URL', plugin_dir_url( __FILE__ ) );
